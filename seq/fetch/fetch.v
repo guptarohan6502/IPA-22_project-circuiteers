1da893c0ad6646ff29952350e8a8489ddf34914b
guptarohan6502 <rohangupta6502@gmail.com>
2022-03-07 13:19:31 +0530

    Fetch completed

--- seq/fetch/fetch.v
+++ seq/fetch/fetch.v
@@ -1,141 +1,105 @@
 // reads instructions from intruction memory 
 // finding values of icode, ifun, rA, rB, valC using the instructions
 
-`timescale 1ns / 1ps
 
-module fetch (clk, pc, 
-                icode, ifun, rA, rB, 
-                ivalid, ierror, valC, valP, out);
+module split(Byte0,icode,ifun);
 
-input clk;
-input [63:0] pc;
+input [7:0] Byte0;
+output [3:0] icode;
+output [3:0] ifun;
 
-output reg [3:0] icode;
-output reg [3:0] ifun;
-
-output reg [3:0] rA;
-output reg [3:0] rB;
-
-output reg ivalid; // instruction validity
-output reg ierror; // instruction error
-
-output reg [63:0] valC;
-output reg [63:0] valP;
-
-output reg out; 
-
-reg [7:0] imem[0:1023]; // instruction memory
-reg [0:79] instruct; 
-
-always@ (posedge clk) begin
-    ierror = 0; 
-    if(pc > 1023)
-    begin 
-        ierror = 1; 
-    end
-
-    instruct = {
-        mem[pc + 0], mem[pc + 1], mem[pc + 2], mem[pc + 3], mem[pc + 4], 
-        mem[pc + 5], mem[pc + 6], mem[pc + 7], mem[pc + 8], mem[pc + 9]
-    };
-
-    icode = instruct[0:3]; 
-    ifun = instruct[4:7];
-    valid = 1'b1; // when icode is invalid, instruction valid = 0
-
-    if(icode == 4'0000) // when halt, icode = 0
-    begin 
-        out = 1; // halt activated 
-        valP = pc + 64'd1;
-    end
-
-    else if(icode == 4'b0001) // for nop, icode = 1
-    begin 
-        valP = pc + 64'd1;
-    end 
-
-    else if(icode == 4'b0010) // for cmovxx, icode = 2
-    begin 
-        rA = instruct[8:11];
-        rB = instruct[12:15];
-        valP = pc + 64'd2;
-    end
-
-    else if(icode == 4'b0011) // for irmovq, icode = 3
-    begin
-        rA = instruct[8:11];
-        rB = instruct[12:15];
-        valC = instruct[16:79];
-        valP = pc + 64'd10;
-    end
-
-    else if(icode == 4'b0100) // for rmmovq, icode = 4
-    begin
-        rA = instruct[8:11];
-        rB = instruct[12:15];
-        valC = instruct[16:79];
-        valP = pc + 64'd10;
-    end
-
-    else if (icode == 4'b0101) // for mrmovq, icode = 5
-    begin
-        rA = instruct[8:11];
-        rB = instruct[12:15];
-        valC = instruct[16:79];
-        valP = pc + 64'd10;
-    end
-
-    else if (icode == 4'b0110) // for OPq, icode = 6
-    begin
-        rA = instruct[8:11];
-        rB = instruct[12:15];
-        valP = pc + 64'd2;
-    end
-
-    else if (icode == 4'b0111) // for jxx, icode = 7
-    begin
-        valC = instruct[8:71];
-        valP = pc + 64'd9;
-    end
-
-    else if (icode == 4'b1000) // for call, icode = 8
-    begin
-        valC = instruct[8:71];
-        valP = pc + 64'd9;
-    end
-
-    else if(icode == 4'b1001) // for ret, icode = 9
-    begin
-        valP = pc + 64'd1;
-    end
-
-    else if (icode == 4'b1010) // for pushq, icode = 10 
-    begin
-        rA = instruct[8:11];
-        rB = instruct[12:15];
-        valP = pc + 64'd2;
-    end
-
-    else if (icode == 4'b1011) // for popq, icode = 11
-    begin
-        rA = instruct[8:11];
-        rB = instruct[12:15];
-        valP = pc + 64'd2;
-    end
-
-   else 
-   begin
-       valid = 1'b0; // icode is invalid 
-   end
-
-end 
+    assign icode = Byte0[0:4];
+    assign ifun = Byte0[5:7];
 
 endmodule
 
 
-module split();
+module Need_VALC(icode,need_valC);
+input [3:0] icode;
+output reg need_valC;
+
+always @(icode)
+    begin
+    case (icode)
+        4'h3, 4'h4, 4'h5, 4'h7, 4'h8,:
+            begin
+                assign need_valC = 1'b1;
+            end
+        4'h1, 4'h2, 4'h6, 4'h6, 4'h9, 4'hA, 4'hB:
+            begin
+                assign need_valC = 1'b0;
+            end
+
+        default: need_valC = 1'b0;
+    endcase
+    end
 endmodule
 
-module align();
+
+module Need_REGIDS(icode,need_regids);
+
+input [3:0] icode;
+output reg need_regids;
+
+    always @(icode)
+        begin
+        case (icode)
+            4'h2, 4'h3, 4'h4, 4'h5, 4'h6, 4'hA, 4'hB,:
+                begin
+                    assign need_regids = 1'b1;
+                end
+            4'h1, 4'h7, 4'h8, 4'h9:
+                begin
+                    assign need_regids = 1'b0;
+                end
+
+            default: need_regids = 1'b0;
+        endcase
+        end
+
 endmodule
 
+
+module align(Byte19,rA,rB,valC);
+
+input [71:0] Byte19;
+output [3:0] rA;
+output [3:0] rB;
+output [63:0] valC;
+
+    assign rA = Byte19[71:68] ;
+    assign rB = Byte19[67:64];
+    assign valC = need_regids ? Byte19[63:0]:Byte19[71:8]
+
+  
+endmodule
+
+module PC_INCREMENT(pc,need_regids,need_valC,valP);
+
+input[63:0] pc;
+input need_regids;
+input need_valC;
+output [63:0] valP;
+
+    assign valP = need_valC ? (need_regids ? pc+10:pc+9):(need_regids ? pc+2:pc+1);
+
+endmodule
+
+module INSTR_VALID(icode,instr_valid);
+input [3:0] icode;
+output reg instr_valid;
+
+always @(icode)
+    begin
+    case (icode)
+        4'h0, 4'h1, 4'h2, 4'h3, 4'h4, 4'h5, 4'h6, 4'h7, 4'h8, 4'h9, 4'hA, 4'hB:
+            begin
+                assign instr_valid = 1'b1;
+            end
+    
+        default: instr_valid = 1'b0;
+    endcase
+    end
+
+
+endmodule
